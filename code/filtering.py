5972d59d11df35609d7453f22d4a7fcdaa723f40
Your Name <you@example.com>
2020-06-01 11:07:06 -0500

    dasdsadwafwetgewhfew

--- code/filtering.py
+++ code/filtering.py
@@ -7,9 +7,9 @@ def load_file(path):
 
     with open(path) as fuse:
         line = fuse.readline()
-
-        for _ in range(1000):  # load the first 1000 point for testing
-
+        # print(line)
+        while line:
+        # for _ in range(100000):
             data.append([float(x) for x in line.split()])
             line = fuse.readline()
 
@@ -24,19 +24,19 @@ def filter1(pts):
     std = np.std(intensity)
 
     threshold = mean + std  # adjust this value to see different results
+    print(f'threshold is {threshold}')
 
     filtered_points = []
 
     for point in pts:
         if point[3] > threshold:
             filtered_points.append(point)
-
+    print(f'after filter 1 shape {np.array(filtered_points).shape}')
     return np.array(filtered_points)
 
 
 def filter2(pts):
-
-    with open('../final_project_data/trajectory.fuse') as tra:
+    with open('../final_project_data/trajectory') as tra:
         data = tra.readline()
         trajectory = []
         while data:
@@ -44,19 +44,28 @@ def filter2(pts):
             data = tra.readline()
 
     trajectory = np.array(trajectory)[:, :3]
+    # print(f'trajectory is {trajectory}')
 
-    threshold = 20  # in meters. adjust this to see different results
+    threshold = 90  # in meters. adjust this to see different results
+
     result = []
     for point in pts:
-        dist = min(np.sum((trajectory-point[:3])**2, axis=1))
+        dist = min(np.sum((trajectory - point[:3]) ** 2, axis=1))**0.5
         if dist < threshold:
             result.append(point)
-
+    print(f'after filter 2 shape {np.array(result).shape}')
     return np.array(result)
 
 
 if __name__ == '__main__':
-    points = load_file('../final_project_data/final_project_point_cloud.fuse')
+    points = load_file('../final_project_data/points')
 
     points = filter1(points)
     points = filter2(points)
+
+    result = pd.DataFrame(points)
+
+    result.to_csv("../final_project_data/filtered_points", sep=" ", header=False, index=False)
+
+
+
--- code/load_data.py
+++ code/load_data.py
@@ -1,16 +1,18 @@
-import pandas as pd 
+import pandas as pd
 from math import *
+
 
 def load_fuse(data_path):
     mylist = []
-    fh = open(data_path,'r')
-    for i,line in enumerate(fh):
+    fh = open(data_path, 'r')
+    for i, line in enumerate(fh):
         lines = []
         for cell in line.split():
             lines.append(cell)
         mylist.append(lines)
     fh.close()
     return mylist
+
 
 def converter(latitude, longitude, height):
     """Return geocentric (Cartesian) Coordinates x, y, z corresponding to
@@ -23,14 +25,15 @@ def converter(latitude, longitude, height):
     φ = radians(latitude)
     λ = radians(longitude)
     sin_φ = sin(φ)
-    a, rf = WGS84           # semi-major axis, reciprocal flattening
+    a, rf = WGS84  # semi-major axis, reciprocal flattening
     e2 = 1 - (1 - 1 / rf) ** 2  # eccentricity squared
-    n = a / sqrt(1 - e2 * sin_φ ** 2) # prime vertical radius
-    r = (n + height) * cos(φ)   # perpendicular distance from z axis
+    n = a / sqrt(1 - e2 * sin_φ ** 2)  # prime vertical radius
+    r = (n + height) * cos(φ)  # perpendicular distance from z axis
     x = r * cos(λ)
     y = r * sin(λ)
     z = (n * (1 - e2) + height) * sin_φ
     return x, y, z
+
 
 def convert_to_cartesian(df_probe):
     idx = 0
@@ -41,28 +44,29 @@ def convert_to_cartesian(df_probe):
         lat = float(lat)
         lon = float(lon)
         alt = float(alt)
-        x,y,z=converter(lat,lon,alt)
-        df_probe.at[idx,'latitude'] = x
-        df_probe.at[idx,'longitude'] = y
-        df_probe.at[idx,'altitude'] = z
-        min_lat = min(min_lat,x)
-        min_lon = min(min_lon,y)
-        min_alt = min(min_alt,z)
-        idx+=1
+        x, y, z = converter(lat, lon, alt)
+        df_probe.at[idx, 'latitude'] = x
+        df_probe.at[idx, 'longitude'] = y
+        df_probe.at[idx, 'altitude'] = z
+        min_lat = min(min_lat, x)
+        min_lon = min(min_lon, y)
+        min_alt = min(min_alt, z)
+        idx += 1
 
     idx = 0
     for lat, lon, alt in zip(df_probe['latitude'], df_probe['longitude'], df_probe['altitude']):
-        df_probe.at[idx,'latitude'] = lat-min_lat
-        df_probe.at[idx,'longitude'] = lon-min_lon
-        df_probe.at[idx,'altitude'] = alt - min_alt
-        idx+=1
+        df_probe.at[idx, 'latitude'] = lat - min_lat
+        df_probe.at[idx, 'longitude'] = lon - min_lon
+        df_probe.at[idx, 'altitude'] = alt - min_alt
+        idx += 1
         # break
     return df_probe
+
 
 if __name__ == "__main__":
     path = "../final_project_data/final_project_point_cloud.fuse"
     result = load_fuse(path)
-    df_result = pd.DataFrame(result, columns = ['latitude','longitude','altitude','intensity']) 
+    df_result = pd.DataFrame(result, columns=['latitude', 'longitude', 'altitude', 'intensity'])
     df_result = convert_to_cartesian(df_result)
     df_result.to_csv("../final_project_data/points", sep=" ", header=False, index=False)
     print(df_result)
