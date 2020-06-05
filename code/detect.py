1924ae12863f8ab668641bdd0d8418040e3a8e83
Your Name <you@example.com>
2020-06-04 22:19:12 -0500

    12321

--- code/detect.py
+++ code/detect.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import DBSCAN
 
-
-img = cv2.imread('points.jpg', 0)
+img = cv2.imread('../points.jpg', 0)
 img_size = img.shape[0]
 
 # edge detect
@@ -13,7 +12,7 @@ img = cv2.medianBlur(img, 3)
 edges = cv2.Canny(img, 10, 20)
 
 plt.subplot(133)
-plt.axis((0,img_size,img_size,0))
+plt.axis((0, img_size, img_size, 0))
 plt.gca().set_aspect('equal', adjustable='box')
 
 # Hough Transform
@@ -24,7 +23,7 @@ width = 2  # width of line marking
 avg_k = 0
 set_b = []
 try:
-    lines = cv2.HoughLinesP(edges,1,np.pi/180,threshold,minLineLength,maxLineGap)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold, minLineLength, maxLineGap)
     for i in range(0, len(lines)):
         # for rho,theta in lines[i]:
         #     a = np.cos(theta)
@@ -36,10 +35,10 @@ try:
         #     x2 = int(x0 - 500*(-b))
         #     y2 = int(y0 - 500*(a))
         for x1, y1, x2, y2 in lines[i]:
-            k = (y2-y1)/(x2-x1)
-            b = y1 - k*x1
-            avg_k += k 
-            plt.plot((x1,x2),(y1,y2))
+            k = (y2 - y1) / (x2 - x1)
+            b = y1 - k * x1
+            avg_k += k
+            plt.plot((x1, x2), (y1, y2))
             # print(b)
 
 except:
@@ -54,6 +53,6 @@ print(avg_k)
 # labels = db.labels_
 # print(np.reshape(labels, [img_size, img_size]))
 
-plt.subplot(131),plt.imshow(img,cmap = 'gray')
-plt.subplot(132),plt.imshow(edges,cmap = 'gray')
+plt.subplot(131), plt.imshow(img, cmap='gray')
+plt.subplot(132), plt.imshow(edges, cmap='gray')
 plt.show()
---
+++ code/link_edges.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+from collections import deque
+import matplotlib.pyplot as plt
+
+image = cv2.imread('points.jpg', cv2.IMREAD_GRAYSCALE)
+
+threshold_high = 90
+threshold_low = 20
+
+image = cv2.copyMakeBorder(image, 1, 1, 1, 1, cv2.BORDER_CONSTANT, value=0)
+
+image = cv2.dilate(image, kernel=np.ones((4, 4)))
+
+image = np.where(image < threshold_low, 0, image)
+image = np.where(image > threshold_low, 255, image)
+
+print(image.shape)
+plt.imshow(image, cmap='gray')
+plt.show()
